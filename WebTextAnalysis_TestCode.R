45bb0d828730aa438816d012aa66df35eb69ae89
vmsmith <redditor.vmsmith@gmail.com>
2017-07-03 09:58:06 -0400

    Added simple node extraction Added text extraction Added identifying distinct nodes from many Added extracting table to data frames

--- WebTextAnalysis_TestCode.R
+++ WebTextAnalysis_TestCode.R
@@ -16,11 +16,41 @@ html_nodes(ateam, "center font b")
 
 # Read in the page
 test1 <- read_html("1_Data/test1.html")
-
 test1
 
+# Simple node extraction --------------------------------------------------
+
+books <- test1 %>% html_nodes("bookstore")
+books          
+books <- test1 %>% html_nodes("bookstore book")
+books
 books <- test1 %>% html_nodes("bookstore book title")
-          
-test1 %>% html_nodes(xpath = "//bookstore//book//title")
+books
+
+books[1]
+
+# Text extraction ---------------------------------------------------------
 
 html_text(books)
+html_text(books[3])
+
+test1 %>% html_nodes("h2") %>%
+  html_text()
+
+# Identifying distinct nodes ----------------------------------------------
+
+
+music <- test1 %>% html_nodes("table#music")
+music
+music <- test1 %>% html_nodes("table#music tr")
+music
+music <- test1 %>% html_nodes("table#music tr td")
+music
+
+# Extracting tables into data frames --------------------------------------
+
+test1 %>%
+  html_node("table#music") %>%
+  html_table(header = NA)
+
+
