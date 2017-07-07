d05d54678f5af5a4d33271e7c4e28563ab043afe
vmsmith <redditor.vmsmith@gmail.com>
2017-07-07 08:33:22 -0400

    Fleshed out <head> and <body> Added two exercises Fleshed out <table> section

--- webtext1.R
+++ webtext1.R
@@ -10,6 +10,14 @@ library(selectr)
 webtext1 <- read_html("1_Data/webtext1.html")
 webtext1
 
+head <- webtext1 %>% html_nodes("head")
+head
+head[[1]]
+
+body <- webtext1 %>% html_nodes("body")
+body
+body[[1]]
+
 # Examine the nodes
 
 wt1nodes <- webtext1 %>% html_nodes("h1")
@@ -29,16 +37,32 @@ wt1nodes
 wt1nodes <- webtext1 %>% html_nodes("h2")
 wt1nodes
 
+str(wt1nodes)
+
+wt1nodes[[1]]
 wt1nodes[1]
+
 wt1nodes[1:3]
 wt1nodes[c(1, 4)]
 
+# Exercise ----------------------------------------------------------------
+
+                          # Isolate the paragraphs
+
 # Extract text ------------------------------------------------------------
 
-wt1nodes <- webtext1 %>% html_nodes("p")
+wt1nodes <- webtext1 %>% html_nodes("h2")
 wt1nodes
 
+html_text(wt1nodes)
+
+html_text(wt1nodes[[1]])
 html_text(wt1nodes[1])
+
+# Exercise ----------------------------------------------------------------
+
+                  # Extract the text from the paragraphs
+
 
 # Extracting from tables --------------------------------------------------
 
@@ -46,13 +70,24 @@ wt1nodes <- webtext1 %>% html_nodes("table")
 wt1nodes
 
 wt1nodes[[1]]
+wt1nodes[1]
+
 wt1nodes[[2]]
+wt1nodes[2]
+
+wt1nodes <- webtext1 %>% html_nodes("table tr")
+
+html_text(wt1nodes[21:24])
+html_text(wt1nodes)
+
+wt1nodes <- webtext1 %>% html_nodes("table tr td")
 
 
 # Extracting from tables into data frames
 
-wt1nodes[[2]] %>% html_table(header = NA)
-
+wt1nodes <- webtext1 %>% html_nodes("table")
+table2_df <- wt1nodes[[2]] %>% html_table(header = NA)
+table2_df
 
 
 
