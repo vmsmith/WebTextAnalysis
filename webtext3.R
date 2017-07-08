e1ac72ad569667e317bae0fbae6997d90ce4c3aa
vmsmith <redditor.vmsmith@gmail.com>
2017-07-08 10:49:41 -0400

    Extended webtext2.R to included classes for row identification

---
+++ webtext3.R
@@ -0,0 +1,34 @@
+# Set up the environment --------------------------------------------------
+
+library(rvest)
+library(magrittr)
+library(selectr)
+
+# Read in the page --------------------------------------------------------
+
+webtext2 <- read_html("1_Data/webtext2.html")
+webtext2
+
+# Select by "class" ----------------------------------------------------
+
+wt2nodes <- webtext2 %>% html_nodes("#music_table")
+wt2nodes[[1]]
+
+wt2nodes <- webtext2 %>% html_nodes("#music_table .american")
+wt2nodes
+wt2nodes[[1]]
+
+wt2nodes <- webtext2 %>% html_nodes("#music_table .british")
+wt2nodes
+wt2nodes[[2]]
+
+wt2nodes <- webtext2 %>% html_nodes("#music_table .american td")
+wt2nodes
+
+html_text(wt2nodes[c(1:5)])
+html_text(wt2nodes[c(1, 6)])
+
+# Exercise ----------------------------------------------------------------
+
+# Extract all the titles from the book table
+# Extract all the information about "Everyday Italian
