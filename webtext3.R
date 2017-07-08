8ced32b785d0119211e292d8e20c5469826e077c
vmsmith <redditor.vmsmith@gmail.com>
2017-07-08 10:51:42 -0400

    Fixed minor typos

--- webtext3.R
+++ webtext3.R
@@ -6,27 +6,27 @@ library(selectr)
 
 # Read in the page --------------------------------------------------------
 
-webtext2 <- read_html("1_Data/webtext2.html")
-webtext2
+webtext3 <- read_html("1_Data/webtext3.html")
+webtext3
 
 # Select by "class" ----------------------------------------------------
 
-wt2nodes <- webtext2 %>% html_nodes("#music_table")
-wt2nodes[[1]]
+wt3nodes <- webtext3 %>% html_nodes("#music_table")
+wt3nodes[[1]]
 
-wt2nodes <- webtext2 %>% html_nodes("#music_table .american")
-wt2nodes
-wt2nodes[[1]]
+wt3nodes <- webtext3 %>% html_nodes("#music_table .american")
+wt3nodes
+wt3nodes[[1]]
 
-wt2nodes <- webtext2 %>% html_nodes("#music_table .british")
-wt2nodes
-wt2nodes[[2]]
+wt3nodes <- webtext3 %>% html_nodes("#music_table .british")
+wt3nodes
+wt3nodes[[2]]
 
-wt2nodes <- webtext2 %>% html_nodes("#music_table .american td")
-wt2nodes
+wt3nodes <- webtext3 %>% html_nodes("#music_table .american td")
+wt3nodes
 
-html_text(wt2nodes[c(1:5)])
-html_text(wt2nodes[c(1, 6)])
+html_text(wt3nodes[c(1:5)])
+html_text(wt3nodes[c(1, 6)])
 
 # Exercise ----------------------------------------------------------------
 
