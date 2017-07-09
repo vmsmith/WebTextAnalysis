d9715cc190215689e0a006d0b9672ebf22fdcd8b
vmsmith <redditor.vmsmith@gmail.com>
2017-07-09 09:17:04 -0400

    Cleaned up and refined <span> example

--- webtext4.R
+++ webtext4.R
@@ -30,22 +30,13 @@ html_text(wt4nodes[c(1, 6)])
 
 # Select by <span class=" "> ----------------------------------------------------
 
-wt4nodes <- webtext4 %>% html_nodes("h2 p .concepts")
+
+wt4nodes <- webtext4 %>% html_nodes(".concepts")
 wt4nodes
 
-wt4nodes <- webtext4 %>% html_nodes(".music #music_table")
-wt4nodes
-wt4nodes[[1]]
+wt4nodes %>% html_text()
 
-wt4nodes <- webtext4 %>% html_nodes(".music #music_table .british")
-wt4nodes
-wt4nodes[[2]]
 
-wt4nodes <- webtext4 %>% html_nodes(".music #music_table .american td")
-wt4nodes
-
-html_text(wt4nodes[c(1:5)])
-html_text(wt4nodes[c(1, 6)])
 
 # Exercise ----------------------------------------------------------------
 
