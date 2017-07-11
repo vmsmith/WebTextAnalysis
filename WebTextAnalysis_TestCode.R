4fb61533be1d7f1e65e294a3412ca27ec79c084b
vmsmith <redditor.vmsmith@gmail.com>
2017-07-11 10:43:28 -0400

    Added link to StackOverflow Changed code to work on Zillow, Norfolk

--- WebTextAnalysis_TestCode.R
+++ WebTextAnalysis_TestCode.R
@@ -1,3 +1,5 @@
+# https://stackoverflow.com/questions/36683510/r-web-scraping-across-multiple-pages
+
 library(rvest)
        
 homes <- lapply(paste0("https://www.zillow.com/homes/for_sale/Norfolk-VA/fsba,fsbo,fore,cmsn_lt/pmf,pf_pt/6210_rid/globalrelevanceex_sort/37.033391,-76.086846,36.756627,-76.435318_rect/11_zm/", 1:10, "_p/"),
